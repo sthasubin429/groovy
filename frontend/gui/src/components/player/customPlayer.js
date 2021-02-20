caa912436f0d56700e986257beba084130ac3008
subin <subinshrestha429@gmail.com>
2021-02-20 22:58:35 +0545

    player controls ui update

--- frontend/gui/src/components/player/customPlayer.js
+++ frontend/gui/src/components/player/customPlayer.js
@@ -1,7 +1,7 @@
 import React, { useState, useRef, useCallback, useEffect } from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlay, faPause } from '@fortawesome/free-solid-svg-icons';
+import { faPlay, faPause, faVolumeUp, faBars, faStepBackward, faStepForward, faRedo, faRandom } from '@fortawesome/free-solid-svg-icons';
 
 function CustomPlayer(props) {
 	const [isPlaying, setIsPlaying] = useState(true);
@@ -34,15 +34,61 @@ function CustomPlayer(props) {
 		}
 	}, [isPlaying]);
 
+	const resetIsPlaying = () => {
+		setIsPlaying(true);
+		playerRef.current.play();
+	};
+
 	return (
 		<>
-			<button onClick={() => props.handleChange('prev')}> Previous</button>
-			<audio autoplay controls src={props.song.song_audio} ref={playerRef} />
-			<button onClick={() => props.handleChange('next')}> Next</button>
-			<h2> {props.song.song_name} </h2>
-			<img src={props.song.song_photo} className='customPlayer-songImg' alt='Song' />
+			<audio autoplay src={props.song.song_audio} ref={playerRef} />
 
-			<FontAwesomeIcon icon={isPlaying ? faPause : faPlay} size='5x' onClick={handlePausePlay} />
+			<div className='customPlayer-container'>
+				<div className='flex-container jc-center customPlayer-header'>
+					<FontAwesomeIcon icon={faVolumeUp} size='2x' />
+					<h2>Now Playing</h2>
+					<FontAwesomeIcon icon={faBars} size='2x' />
+				</div>
+
+				<div className='flex-container jc-center'>
+					<img src={props.song.song_photo} className='customPlayer-songImg' alt='Song' />
+				</div>
+
+				<div className='flex-container jc-center'>
+					<h2> {props.song.song_name} </h2>
+				</div>
+				<div className='flex-container jc-center'>
+					<h4> {props.song.username} </h4>
+				</div>
+
+				<div className='flex-container jc-center ai-center customPlayer-controls'>
+					<FontAwesomeIcon icon={faRedo} size='4x' />
+
+					<div className='main-controls'>
+						<FontAwesomeIcon
+							icon={faStepBackward}
+							size='4x'
+							onClick={() => {
+								props.handleChange('prev');
+								resetIsPlaying();
+							}}
+						/>
+
+						<FontAwesomeIcon icon={isPlaying ? faPause : faPlay} size='4x' onClick={handlePausePlay} />
+
+						<FontAwesomeIcon
+							icon={faStepForward}
+							size='4x'
+							onClick={() => {
+								props.handleChange('next');
+								resetIsPlaying();
+							}}
+						/>
+					</div>
+
+					<FontAwesomeIcon icon={faRandom} size='4x' />
+				</div>
+			</div>
 		</>
 	);
 }
