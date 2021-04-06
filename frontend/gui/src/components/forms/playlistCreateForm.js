42a4bcd026b0981ee6666164b9f85589dec74439
subin <subinshrestha429@gmail.com>
2021-04-06 10:57:48 +0545

    playlist creation for with multiple songs with add and remove

--- frontend/gui/src/components/forms/playlistCreateForm.js
+++ frontend/gui/src/components/forms/playlistCreateForm.js
@@ -6,8 +6,9 @@ import { getUserDetails } from '../../store/actions/user.js';
 
 export default function PlaylistCreateForm(props) {
 	const [options, setOptions] = useState([]);
-	let userData = getUserDetails();
 	const choiceRef = useRef();
+	const [loading, setLoading] = useState(false);
+	let userData = getUserDetails();
 
 	useEffect(() => {
 		// console.log(typeof props.songs);
@@ -35,22 +36,26 @@ export default function PlaylistCreateForm(props) {
 
 	const handleSubmit = (event, requestType) => {
 		event.preventDefault();
-
+		setLoading(true);
 		let formData = new FormData();
 		formData.append('playlist_name', event.target.elements.playlistName.value);
 		formData.append('created_by', userData.userInfo.pk);
 
 		const token = localStorage.getItem('token');
 
-		let songChoice = event.target.elements.songChoice.value;
-		let songId = null;
+		let songChoice = event.target.elements.songChoice;
+		// console.log(songChoice);
+		let songIdArr = [];
 		const songs = [...props.songs];
 
 		songs.forEach((song) => {
-			if (song['song_name'] === songChoice) {
-				songId = song.id;
-			}
+			songChoice.forEach((choice) => {
+				if (song['song_name'] === choice.value) {
+					songIdArr.push(song.id);
+				}
+			});
 		});
+		// console.log(songIdArr);
 
 		switch (requestType) {
 			case POST:
@@ -63,7 +68,10 @@ export default function PlaylistCreateForm(props) {
 						})
 						.then((res) => {
 							let playlistDetail = res.data;
-							addSongs(songId, playlistDetail, POST);
+
+							songIdArr.forEach((songId) => {
+								addSongs(songId, playlistDetail, POST);
+							});
 						})
 						.catch((err) => console.log(err));
 				}
@@ -86,13 +94,41 @@ export default function PlaylistCreateForm(props) {
 							},
 						})
 						.then((res) => {
-							console.log(res.data);
+							setLoading(true);
 							window.location.replace('http://localhost:3000/playlist');
 						})
 						.catch((err) => console.log(err));
 				}
 		}
 	};
+	const [choiceKeys, setChoiceKeys] = useState([0, 1]);
+
+	const [songChoiceArray, setSongChoiceArray] = useState([]);
+	useEffect(() => {
+		let temp = [];
+
+		choiceKeys.forEach((keys) =>
+			temp.push(
+				<div className='form-group col-md-10' key={keys}>
+					<input className='form-control' list='songList' id='songChoice' placeholder='Type to search...' ref={choiceRef} />
+					<button
+						onClick={(event) => {
+							event.preventDefault();
+							if (choiceKeys.length > 2) {
+								let tempChoice = [...choiceKeys];
+								tempChoice.splice(tempChoice.indexOf(keys), 1);
+								setChoiceKeys(tempChoice);
+							}
+						}}
+					>
+						Remove
+					</button>
+					<datalist id='songList'> {options} </datalist>
+				</div>
+			)
+		);
+		setSongChoiceArray(temp);
+	}, [choiceKeys, options]);
 
 	return (
 		<>
@@ -112,16 +148,24 @@ export default function PlaylistCreateForm(props) {
 					<h3> Add Songs</h3>
 				</div>
 
-				<div className='form-group col-md-10'>
-					{/* <select id='songChoice' class='form-control'>
-						{options}
-					</select> */}
+				{songChoiceArray}
 
-					<input className='form-control' list='songList' id='songChoice' placeholder='Type to search...' ref={choiceRef} />
-					<datalist id='songList'> {options} </datalist>
-				</div>
+				<button
+					onClick={(event) => {
+						event.preventDefault();
+						setChoiceKeys(choiceKeys.concat(choiceKeys[choiceKeys.length - 1] + 1));
+					}}
+				>
+					Add
+				</button>
 
-				<input type='Submit' name='submit' className='btn btn-primary' />
+				{loading ? (
+					<div className='spinner-border text-primary' role='status'>
+						<span className='sr-only'>Loading...</span>
+					</div>
+				) : (
+					<input type='Submit' name='submit' className='btn btn-primary' />
+				)}
 			</form>
 		</>
 	);
