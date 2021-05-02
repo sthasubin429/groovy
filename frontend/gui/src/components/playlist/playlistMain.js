638d9c9d1f62dec1497e82beb90d0c9d95f096a6
subin <subinshrestha429@gmail.com>
2021-05-02 22:53:11 +0545

    pagination added in playlist

--- frontend/gui/src/components/playlist/playlistMain.js
+++ frontend/gui/src/components/playlist/playlistMain.js
@@ -28,20 +28,97 @@ const PlaylistCard = (props) => {
 		</>
 	);
 };
+
+// const RenderPaginaiton = (props) => {
+// 	return (
+
+// 	);
+// };
+
 export default function PlaylistMain(props) {
 	const [playlist, setPlaylist] = useState(props.playlist);
 	// const dispatch = useDispatch();
-	// console.log(playlist);
+	const [playlistPaginated, setPlaylistPaginated] = useState([]);
+	const [currentPage, setCurrentPage] = useState(1);
+	let noOfItem = 4;
+	let noOfPages = parseInt(Math.floor(playlist.length / noOfItem) + 1);
+
+	// console.log(noOfPages);
 
 	useEffect(() => {
 		setPlaylist(props.playlist);
 	}, [props.playlist]);
 
+	useEffect(() => {
+		let filterd = playlist.slice((currentPage - 1) * noOfItem, currentPage * noOfItem);
+		console.log(currentPage);
+		setPlaylistPaginated(filterd);
+	}, [currentPage, playlist]);
+
+	const changePlaylist = (direction) => {
+		if (direction === 'prev') {
+			if (currentPage !== 1) {
+				setCurrentPage(currentPage - 1);
+			} else {
+				setCurrentPage(currentPage);
+			}
+		} else if (direction === 'next') {
+			if (currentPage !== noOfPages) {
+				setCurrentPage(currentPage + 1);
+			} else {
+				setCurrentPage(currentPage);
+			}
+		}
+	};
+
 	return (
 		<>
 			<div className='playlist-main-container d-flex align-content-between justify-content-start flex-wrap'>
-				<PlaylistCard playlist={playlist} />
+				<PlaylistCard playlist={playlistPaginated} />
 			</div>
+
+			{/* <RenderPaginaiton numPages={noOfPages} /> */}
+			<ul className='pagination d-flex justify-content-center'>
+				<li className='page-item'>
+					<button
+						className='page-link'
+						onClick={(e) => {
+							e.preventDefault();
+							changePlaylist('prev');
+						}}
+					>
+						Previous
+					</button>
+				</li>
+				{[...Array(noOfPages)].map((e, i) => {
+					return (
+						<li className={i + 1 == currentPage ? 'page-item active' : 'page-item'} key={i}>
+							<button
+								className='page-link'
+								onClick={(e) => {
+									e.preventDefault();
+									setCurrentPage(i + 1);
+								}}
+							>
+								{i + 1}
+							</button>
+						</li>
+					);
+				})}
+
+				<li className='page-item'>
+					<button
+						className='page-link'
+						onClick={(e) => {
+							e.preventDefault();
+							changePlaylist('next');
+						}}
+					>
+						Previous
+					</button>
+				</li>
+			</ul>
+			<ul className='d-flex justify-content-center'></ul>
 		</>
 	);
 }
